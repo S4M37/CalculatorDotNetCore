c0be7a95c25b175399e359a42a006cd72c646ba5
Samet Oussema <sametoussema@gmail.com>
2017-01-18 00:48:27 +0100

    add unit test for Arithmetic service class

---
+++ src/CalculatorDotnetCore/Services/Arithmetic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorDotnetCore.Services
+{
+    public class Arithmetic
+    {
+        public double add(double a, double b)
+        {
+            return 0;
+        }
+        public double multiplicate(double a, double b)
+        {
+            return 0;
+        }
+        public double subtract(double a, double b)
+        {
+            return 0;
+        }
+        public double divide(double a, double b)
+        {
+            return 0;
+        }
+    }
+}
---
+++ test/XUnitTest/ArithmeticTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using CalculatorDotnetCore.Services;
+
+namespace XUnitTest
+{
+    public class ArithmeticTest
+    {
+        public Arithmetic op;
+        public ArithmeticTest()
+        {
+            op = new Arithmetic();
+        }
+        [Fact]
+        public void AddTest()
+        {
+            double expected = 7;
+            double actual = op.add(4,3);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void MultiplicateTest()
+        {
+            double expected = 1;
+            double actual = op.multiplicate(4, 3);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void SubtractTest()
+        {
+            double expected = 7;
+            double actual = op.subtract(4, 3);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void DivideTest()
+        {
+            double expected = 1.5;
+            double actual = op.divide(3, 2);
+
+            Assert.Equal(expected, actual);
+        }
+
+
+    }
+}
